195cc1a68b43190f70bf1ec534d6fbb99ad0536c
allen-xu <tomato_xu@outlook.com>
2025-08-28 18:26:45 +0800

    Update editCodeIntent2.ts

--- src/extension/intents/node/editCodeIntent2.ts
+++ src/extension/intents/node/editCodeIntent2.ts
@@ -81,7 +81,7 @@ export class EditCode2Intent extends EditCodeIntent {
 
 	protected override getIntentHandlerOptions(request: vscode.ChatRequest): IDefaultIntentRequestHandlerOptions | undefined {
 		return {
-			maxToolCallIterations: getRequestedToolCallIterationLimit(request) ?? this.configurationService.getNonExtensionConfig('chat.agent.maxRequests') ?? 15,
+			maxToolCallIterations: getRequestedToolCallIterationLimit(request) ?? this.configurationService.getNonExtensionConfig('chat.agent.maxRequests') ?? 150,
 			temperature: this.configurationService.getConfig(ConfigKey.Internal.AgentTemperature) ?? 0,
 			overrideRequestLocation: ChatLocation.EditingSession,
 		};
